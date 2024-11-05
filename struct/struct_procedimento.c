a7f7a7a9f6bf3d27bc8e4d0c8553a03b7b1c39c4
Guizin <buenovictorsantana@gmail.com>
2024-11-05 09:54:58 -0300

    Add files via upload

---
+++ struct/struct_procedimento.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+struct aluno_ia24{
+
+    char nome[20];
+    char genero[20];
+    int idade;
+    char email[50];
+    int matricula;
+};
+
+void preenche(){
+
+    struct aluno_ia24 novo;
+
+    printf("\nDiga o nome do(a) aluno: ");
+    fgets(novo.nome, sizeof(novo.nome), stdin);
+
+    printf("\nDiga o genero do(a) %s: ", novo.nome);
+    fgets(novo.genero, sizeof(novo.genero), stdin);
+
+    printf("\nDigite o email do(a) %s: ", novo.nome);
+    fgets(novo.email, sizeof(novo.email), stdin);
+
+    printf("\nDigite a idade do(a) %s: ", novo.nome);
+    scanf("%d", &novo.idade);
+
+    printf("\nDigite a matricula do(a) %s: \n", novo.nome);
+    scanf("%d", &novo.matricula);
+
+    printf("O(a) aluno(a) %s eh %s, seu email eh %s, sua matricula eh %d e ele(a) tem %d anos de idade\n", novo.nome, novo.genero, novo.email, novo.matricula, novo.idade);
+}
+
+int main(void){
+
+    preenche();
+}
